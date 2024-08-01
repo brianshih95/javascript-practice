741662b9d6f965f7b6b1883191c356e68ae7561c
brianshih59 <brianshih59@gmail.com>
2024-08-01 16:57:49 +0800

    Generate HTML for the order summary

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,14 @@
-export const cart = [];
+export const cart = [{
+  productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+  quantity: 2
+}];
 
 export function addToCart(productId) {
   let matchingItem = '';
   cart.forEach((cartItem) => {
-    if(productId === cartItem.productId){
+    if (productId === cartItem.productId) {
       matchingItem = cartItem;
+      return;
     }
   });
 
